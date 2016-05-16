cb527781cbb1cd0bfc7a4daca78a15c97d46a317
cpg6b <chris.garvey@gmail.com>
2016-05-15 20:28:16 -0700

    feat: Add tableview

--- TuneSentry/ArtistSearchViewController.swift
+++ TuneSentry/ArtistSearchViewController.swift
@@ -13,6 +13,7 @@ class ArtistSearchViewController: UIViewController {
     // MARK: - Properties
     
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     
     
     // MARK: - Life Cycle
@@ -25,15 +26,12 @@ class ArtistSearchViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
 
+
+// MARK: - Search Bar Delegate
 
 extension ArtistSearchViewController: UISearchBarDelegate {
     
@@ -44,11 +42,54 @@ extension ArtistSearchViewController: UISearchBarDelegate {
     func performSearch(searchText: String) {
         
         print(searchText)
+        
         // TO DO: add search functionality
         
+        
+        tableView.reloadData()
         searchBar.resignFirstResponder()
     }
 
+}
+
+
+// MARK: - Table View Data Source Methods
+
+extension ArtistSearchViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // return a cell depending on what the search state is...
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+}
+
+
+// MARK: - Table View Delegate Methods
+
+extension ArtistSearchViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        searchBar.resignFirstResponder()
+        
+        // TO DO: add the artist in that row to the artist watch list
+        // TO DO: use HUD to indicate to user the artist was successfully added
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        
+        // check the search state... if it hasn't searched yet, is loading, or didn't return any results then return nil
+        return nil
+        
+        // otherwise if there are results, then allow the user to select those results...
+    }
+    
+    
 }
 
 
