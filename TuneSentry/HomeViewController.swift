5303e5e375750ce1bc76cedbdf0bfcc9bc7cdb77
cpg6b <chris.garvey@gmail.com>
2016-05-29 18:28:20 -0700

    feat: Add flow layouts for collection views

--- TuneSentry/HomeViewController.swift
+++ TuneSentry/HomeViewController.swift
@@ -16,6 +16,7 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var artistCollectionView: UICollectionView!
     
     var newReleases = true
+    var watchlistArtists = [Artist]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +32,8 @@ class HomeViewController: UIViewController {
         newReleaseCollectionView.registerNib(cellNib, forCellWithReuseIdentifier: CollectionViewCellIdentifiers.noNewReleasesCell)
         
         // load the nibs for the artist collection view
-        
+        cellNib = UINib(nibName: CollectionViewCellIdentifiers.artistCell, bundle: nil)
+        artistCollectionView.registerNib(cellNib, forCellWithReuseIdentifier: CollectionViewCellIdentifiers.artistCell)
         
     }
 
@@ -39,24 +41,41 @@ class HomeViewController: UIViewController {
     struct CollectionViewCellIdentifiers {
         static let newReleaseCell = "NewReleaseCell"
         static let noNewReleasesCell = "NoNewReleasesCell"
+        static let artistCell = "ArtistCell"
     }
     
     
-    // MARK: Layout
+    // MARK: Collection View Layouts
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        // set up the new releases collection view
         let newReleaseLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         newReleaseLayout.sectionInset = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
         newReleaseLayout.minimumInteritemSpacing = 5
         newReleaseLayout.scrollDirection = .Horizontal
         
-        let height = 124
-        let width = newReleases ? 100 : 238
+        let newReleaseHeight = 124
+        let newReleaseWidth = newReleases ? 100 : 238
         
-        newReleaseLayout.itemSize = CGSize(width: width, height: height)
+        newReleaseLayout.itemSize = CGSize(width: newReleaseWidth, height: newReleaseHeight)
         newReleaseCollectionView.collectionViewLayout = newReleaseLayout
+        
+        // set up the artists collection view
+        let artistLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        artistLayout.sectionInset = UIEdgeInsets(top: 5, left: 7, bottom: 0, right: 7)
+        artistLayout.minimumInteritemSpacing = 8
+        artistLayout.minimumLineSpacing = 6
+        artistLayout.scrollDirection = .Vertical
+        
+        print(artistCollectionView.frame.size.width)
+        let artistCollectionViewCellHeight = 182
+        let artistCollectionViewCellWidth = Int((artistCollectionView.frame.size.width - 30) / 3)
+        
+        artistLayout.itemSize = CGSize(width: artistCollectionViewCellWidth, height: artistCollectionViewCellHeight)
+        artistCollectionView.collectionViewLayout = artistLayout
+        
     }
     
     
@@ -71,10 +90,18 @@ extension HomeViewController: UICollectionViewDelegate {
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        if !newReleases {
-            return 1
+        if collectionView == newReleaseCollectionView {
+            if !newReleases {
+                return 1
+            } else {
+                return 10
+            }
         } else {
-            return 10
+            if watchlistArtists.isEmpty {
+                return 1
+            } else {
+                return 25
+            }
         }
     }
 }
@@ -84,15 +111,24 @@ extension HomeViewController: UICollectionViewDataSource {
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
-        var cell = UICollectionViewCell()
         
-        if !newReleases {
-            cell = collectionView.dequeueReusableCellWithReuseIdentifier(CollectionViewCellIdentifiers.noNewReleasesCell, forIndexPath: indexPath)
+        if collectionView == newReleaseCollectionView {
+            
+            if !newReleases {
+                return newReleaseCollectionView.dequeueReusableCellWithReuseIdentifier(CollectionViewCellIdentifiers.noNewReleasesCell, forIndexPath: indexPath)
+            } else {
+                return newReleaseCollectionView.dequeueReusableCellWithReuseIdentifier(CollectionViewCellIdentifiers.newReleaseCell, forIndexPath: indexPath)
+            }
+            
         } else {
-            cell = collectionView.dequeueReusableCellWithReuseIdentifier(CollectionViewCellIdentifiers.newReleaseCell, forIndexPath: indexPath)
+            
+            if watchlistArtists.isEmpty {
+                // show the cell that says no artists watching right now...
+                return UICollectionViewCell()
+            } else {
+                return artistCollectionView.dequeueReusableCellWithReuseIdentifier(CollectionViewCellIdentifiers.artistCell, forIndexPath: indexPath)
+            }
+            
         }
-        
-        return cell
-        
     }
 }
